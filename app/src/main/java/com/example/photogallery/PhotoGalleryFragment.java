09e3b3b9f2a0e801434fa59dbde963f6ab960705
Ali-Mostaghimi <52744015+Ali-Mostaghimi@users.noreply.github.com>
2020-04-05 13:07:06 +0430

    Initial commit.

---
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,39 @@
+package com.example.photogallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    public static PhotoGalleryFragment newInstance(){
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mRecyclerView = view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package com.example.photogallery;
+
+import android.os.Bundle;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+        }
+    }
+}
